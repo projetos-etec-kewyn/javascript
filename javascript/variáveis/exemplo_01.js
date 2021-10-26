c79eed4feedf585d24316df3c362e6862d45e873
Kewyn Willyam Souza Torres <79849103+projetos-etec-kewyn@users.noreply.github.com>
2021-10-26 09:35:07 -0300

    Delete javascript directory

--- javascript/variáveis/exemplo_01.js
+++
@@ -1,8 +0,0 @@
-var num1 = 25;
-var num2 = 26;
-soma = num1 + num2;
-texto = "Vamos criar uma calculadora";
-document.getElementById('teste').innerHTML = texto;
-alert(soma);
-//document.write(texto);
-console.log(num1);
